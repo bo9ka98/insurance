c80200f90b0bed01a9261b8a4294382ff5093c05
bo9ka98 <bo9ka98@gmail.com>
2019-01-07 07:06:01 +0300

    add entity

---
+++ WindowsFormsApp2/Alias.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class Alias
+    {
+        public string Surname { get; set; }
+        private string Name { get; set; }
+        private string MiddleName { get; set; }
+    }
+}
---
+++ WindowsFormsApp2/LegalСlient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class LegalСlient : Companu, IClient
+    {
+        void IClient.SetContract()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
