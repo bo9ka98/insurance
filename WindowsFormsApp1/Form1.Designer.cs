a9721666cefcac394b159882f1ef15b04d86fb50
vitaliy.solovchuk <bo9ka98@gmail.com>
2019-01-07 05:56:44 +0300

    first

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -30,11 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
+            this.pass = new System.Windows.Forms.TextBox();
             this.login = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.listBoxStringCollect = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listStrCollect = new System.Windows.Forms.ListBox();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
             this.radioButton3 = new System.Windows.Forms.RadioButton();
@@ -61,7 +61,7 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.pass);
             this.groupBox1.Controls.Add(this.login);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
@@ -70,6 +70,32 @@
             this.groupBox1.Size = new System.Drawing.Size(200, 76);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(49, 49);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(100, 20);
+            this.pass.TabIndex = 3;
+            this.pass.TextChanged += new System.EventHandler(this.pass_TextChanged);
+            // 
+            // login
+            // 
+            this.login.Location = new System.Drawing.Point(48, 17);
+            this.login.Name = "login";
+            this.login.Size = new System.Drawing.Size(100, 20);
+            this.login.TabIndex = 2;
+            this.login.TextChanged += new System.EventHandler(this.login_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "pass";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label2
             // 
@@ -81,48 +107,24 @@
             this.label2.Text = "login";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // label3
+            // listStrCollect
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(7, 49);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(29, 13);
-            this.label3.TabIndex = 1;
-            this.label3.Text = "pass";
-            // 
-            // login
-            // 
-            this.login.Location = new System.Drawing.Point(48, 17);
-            this.login.Name = "login";
-            this.login.Size = new System.Drawing.Size(100, 20);
-            this.login.TabIndex = 2;
-            this.login.TextChanged += new System.EventHandler(this.login_TextChanged);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(49, 49);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 20);
-            this.textBox2.TabIndex = 3;
-            // 
-            // listBoxStringCollect
-            // 
-            this.listBoxStringCollect.FormattingEnabled = true;
-            this.listBoxStringCollect.Items.AddRange(new object[] {
+            this.listStrCollect.FormattingEnabled = true;
+            this.listStrCollect.Items.AddRange(new object[] {
             "строка1",
             "строка2",
             "строка3",
             "строка4"});
-            this.listBoxStringCollect.Location = new System.Drawing.Point(126, 200);
-            this.listBoxStringCollect.Name = "listBoxStringCollect";
-            this.listBoxStringCollect.Size = new System.Drawing.Size(120, 95);
-            this.listBoxStringCollect.TabIndex = 2;
-            this.listBoxStringCollect.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listStrCollect.Location = new System.Drawing.Point(106, 174);
+            this.listStrCollect.Name = "listStrCollect";
+            this.listStrCollect.Size = new System.Drawing.Size(120, 95);
+            this.listStrCollect.TabIndex = 2;
+            this.listStrCollect.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(88, 315);
+            this.radioButton1.Location = new System.Drawing.Point(88, 301);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(85, 17);
             this.radioButton1.TabIndex = 3;
@@ -134,7 +136,7 @@
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(88, 338);
+            this.radioButton2.Location = new System.Drawing.Point(88, 324);
             this.radioButton2.Name = "radioButton2";
             this.radioButton2.Size = new System.Drawing.Size(85, 17);
             this.radioButton2.TabIndex = 4;
@@ -145,7 +147,7 @@
             // radioButton3
             // 
             this.radioButton3.AutoSize = true;
-            this.radioButton3.Location = new System.Drawing.Point(88, 362);
+            this.radioButton3.Location = new System.Drawing.Point(88, 348);
             this.radioButton3.Name = "radioButton3";
             this.radioButton3.Size = new System.Drawing.Size(85, 17);
             this.radioButton3.TabIndex = 5;
@@ -168,7 +170,7 @@
             // radioButton5
             // 
             this.radioButton5.AutoSize = true;
-            this.radioButton5.Location = new System.Drawing.Point(15, 53);
+            this.radioButton5.Location = new System.Drawing.Point(15, 47);
             this.radioButton5.Name = "radioButton5";
             this.radioButton5.Size = new System.Drawing.Size(85, 17);
             this.radioButton5.TabIndex = 7;
@@ -183,7 +185,7 @@
             this.groupBox2.Controls.Add(this.radioButton5);
             this.groupBox2.Location = new System.Drawing.Point(349, 76);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(201, 92);
+            this.groupBox2.Size = new System.Drawing.Size(201, 76);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "groupBox2";
@@ -191,7 +193,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(364, 218);
+            this.checkBox1.Location = new System.Drawing.Point(364, 162);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 9;
@@ -201,7 +203,7 @@
             // checkBox2
             // 
             this.checkBox2.AutoSize = true;
-            this.checkBox2.Location = new System.Drawing.Point(364, 242);
+            this.checkBox2.Location = new System.Drawing.Point(364, 186);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(80, 17);
             this.checkBox2.TabIndex = 10;
@@ -211,7 +213,7 @@
             // checkBox3
             // 
             this.checkBox3.AutoSize = true;
-            this.checkBox3.Location = new System.Drawing.Point(364, 266);
+            this.checkBox3.Location = new System.Drawing.Point(364, 210);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(80, 17);
             this.checkBox3.TabIndex = 11;
@@ -220,12 +222,13 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(369, 338);
+            this.button1.Location = new System.Drawing.Point(349, 295);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 12;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
@@ -240,7 +243,7 @@
             this.Controls.Add(this.radioButton3);
             this.Controls.Add(this.radioButton2);
             this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.listBoxStringCollect);
+            this.Controls.Add(this.listStrCollect);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -259,11 +262,11 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox pass;
         private System.Windows.Forms.TextBox login;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBoxStringCollect;
+        private System.Windows.Forms.ListBox listStrCollect;
         private System.Windows.Forms.RadioButton radioButton1;
         private System.Windows.Forms.RadioButton radioButton2;
         private System.Windows.Forms.RadioButton radioButton3;
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -32,6 +32,11 @@ namespace WindowsFormsApp1
 
         }
 
+        private void pass_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void label2_Click(object sender, EventArgs e)
         {
 
@@ -39,10 +44,21 @@ namespace WindowsFormsApp1
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            string text = listStrCollect.GetItemText(listStrCollect.SelectedItem);
+            MessageBox.Show(text);
 
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(login.Text + pass.Text);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
         {
 
         }
