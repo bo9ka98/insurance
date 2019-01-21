a60ec49a72351a889c529fb832a0d43b95bf909e
bo9ka98 <bo9ka98@gmail.com>
2019-01-21 12:19:35 +0300

    new forms categori and cases

---
+++ WindowsFormsApp1/NewCategoriPoliceForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace View
+{
+    partial class NewCategoriPoliceForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tB_policyCost = new System.Windows.Forms.TextBox();
+            this.tB_policyName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.l_Info = new System.Windows.Forms.Label();
+            this.b_Registration = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tB_policyCost
+            // 
+            this.tB_policyCost.Location = new System.Drawing.Point(209, 130);
+            this.tB_policyCost.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_policyCost.Name = "tB_policyCost";
+            this.tB_policyCost.Size = new System.Drawing.Size(264, 20);
+            this.tB_policyCost.TabIndex = 13;
+            this.tB_policyCost.TextChanged += new System.EventHandler(this.policyCostTextBox_TextChanged);
+            // 
+            // tB_policyName
+            // 
+            this.tB_policyName.Location = new System.Drawing.Point(209, 90);
+            this.tB_policyName.Margin = new System.Windows.Forms.Padding(2);
+            this.tB_policyName.Name = "tB_policyName";
+            this.tB_policyName.Size = new System.Drawing.Size(264, 20);
+            this.tB_policyName.TabIndex = 12;
+            this.tB_policyName.TextChanged += new System.EventHandler(this.policyNameTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 93);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Название полиса";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Макс стоимость";
+            // 
+            // l_Info
+            // 
+            this.l_Info.AutoSize = true;
+            this.l_Info.Location = new System.Drawing.Point(144, 46);
+            this.l_Info.Name = "l_Info";
+            this.l_Info.Size = new System.Drawing.Size(205, 13);
+            this.l_Info.TabIndex = 15;
+            this.l_Info.Text = "Регистрация новой категории полисов";
+            // 
+            // b_Registration
+            // 
+            this.b_Registration.Location = new System.Drawing.Point(431, 187);
+            this.b_Registration.Name = "b_Registration";
+            this.b_Registration.Size = new System.Drawing.Size(75, 23);
+            this.b_Registration.TabIndex = 16;
+            this.b_Registration.Text = "Registration";
+            this.b_Registration.UseVisualStyleBackColor = true;
+            // 
+            // NewCategoriPoliceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 236);
+            this.Controls.Add(this.b_Registration);
+            this.Controls.Add(this.l_Info);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tB_policyCost);
+            this.Controls.Add(this.tB_policyName);
+            this.Controls.Add(this.label1);
+            this.Name = "NewCategoriPoliceForm";
+            this.Text = "NewCategoriPoliceForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tB_policyCost;
+        private System.Windows.Forms.TextBox tB_policyName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label l_Info;
+        private System.Windows.Forms.Button b_Registration;
+    }
+}
---
+++ WindowsFormsApp1/NewInsuranceCasesForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace View
+{
+    partial class NewInsuranceCasesForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.paymPercentTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.insCaseTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.b_Registration = new System.Windows.Forms.Button();
+            this.l_razmern = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // paymPercentTextBox
+            // 
+            this.paymPercentTextBox.Location = new System.Drawing.Point(205, 133);
+            this.paymPercentTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.paymPercentTextBox.Name = "paymPercentTextBox";
+            this.paymPercentTextBox.Size = new System.Drawing.Size(236, 20);
+            this.paymPercentTextBox.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Процент выплаты";
+            // 
+            // insCaseTextBox
+            // 
+            this.insCaseTextBox.Location = new System.Drawing.Point(205, 92);
+            this.insCaseTextBox.Margin = new System.Windows.Forms.Padding(2);
+            this.insCaseTextBox.Name = "insCaseTextBox";
+            this.insCaseTextBox.Size = new System.Drawing.Size(264, 20);
+            this.insCaseTextBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Регистрация нового страхового случая";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 95);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Название случая";
+            // 
+            // b_Registration
+            // 
+            this.b_Registration.Location = new System.Drawing.Point(441, 195);
+            this.b_Registration.Name = "b_Registration";
+            this.b_Registration.Size = new System.Drawing.Size(75, 23);
+            this.b_Registration.TabIndex = 17;
+            this.b_Registration.Text = "Registration";
+            this.b_Registration.UseVisualStyleBackColor = true;
+            // 
+            // l_razmern
+            // 
+            this.l_razmern.AutoSize = true;
+            this.l_razmern.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.l_razmern.Location = new System.Drawing.Point(446, 128);
+            this.l_razmern.Name = "l_razmern";
+            this.l_razmern.Size = new System.Drawing.Size(25, 24);
+            this.l_razmern.TabIndex = 18;
+            this.l_razmern.Text = "%";
+            // 
+            // NewInsuranceCasesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 236);
+            this.Controls.Add(this.l_razmern);
+            this.Controls.Add(this.b_Registration);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.paymPercentTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.insCaseTextBox);
+            this.Name = "NewInsuranceCasesForm";
+            this.Text = "NewInsuranceCasesForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox paymPercentTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox insCaseTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button b_Registration;
+        private System.Windows.Forms.Label l_razmern;
+    }
+}
